d6c68956aa9a478c63c84e66b427c8c614b5a0b0
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-09-02 00:46:50 +0530

    condtionally rendering effects

--- src/componets/HooksEffect1.js
+++ src/componets/HooksEffect1.js
@@ -2,11 +2,18 @@ import React,{useEffect,useState} from 'react'
 
 function HooksEffect1() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
+    //for condtionallly running effect add second parameter and in that array what varaible to watch for 
     useEffect(() => {
+        console.log('updating document title ')
        document.title=`you clicked ${count}  timess`
-    })
+    },[count])
     return (
         <div>
+            <input type="text"
+                value={name} 
+                onChange={e=>setName(e.target.value)}
+                />
             <button onClick={()=>setCount(count +1)}>Click {count } times</button>  
         </div>
     )
